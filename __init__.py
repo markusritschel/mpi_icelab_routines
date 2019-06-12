c0089db77fd0f02f738e4f18b6e3769e30927c57
markusritschel <github@markusritschel.de>
2019-06-12 16:57:12 +0200

    import subroutines

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
- 
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  kontakt@markusritschel.de
+# Date:   03/04/2019
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+from __future__ import division, print_function, absolute_import
+
+from mpi_icelab_routines.arduino import read_arduino
+from mpi_icelab_routines.ctds import read_ctd
+from mpi_icelab_routines.harps.salinity_harps import read as read_harp
+from mpi_icelab_routines.licor import read_licor
